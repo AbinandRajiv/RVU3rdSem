afc54916b0becff5291c36d0b5b4806fa3631888
AbinandRajiv <abinandrajivthichuvalappilbca24@rvu.edu.in>
2025-08-26 10:34:32 +0530

    Create q2.java

---
+++ Java(cs2053)/lab3/q2.java
@@ -0,0 +1,17 @@
+class Calc {
+	   int add(int a, int b) {
+	       return a + b;
+	   }
+	    double add(double a, double b) {
+	        return a + b;
+	    }
+	}
+	public class q3 {
+	    public static void main(String[] args) {
+	        Calculator calc = new Calculator();
+
+	        System.out.println("Sum of 2: " + calc.add(10, 20));
+	        System.out.println("Sum of doubles: " + calc.add(5.5, 4.5));
+	    }
+	}
+  
